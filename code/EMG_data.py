88a8dfce2237fabbe6d7537d27e22310084b69ef
Andru Liu <andruliu2022@u.northwestern.edu>
2022-02-20 02:12:41 -0600

    Recorded 10 emg data samples

--- code/EMG_data.py
+++ code/EMG_data.py
@@ -5,7 +5,7 @@ import csv
 arduino_port = "/dev/ttyACM0"
 # baud = 115200
 baud = 9600
-fileName = "./data/Combined_EMG_data1.csv"
+fileName = "./data/Combined_EMG_data10.csv"
 samples = 100
 # print_labels = False
 i = 0
