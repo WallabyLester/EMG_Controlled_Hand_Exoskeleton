1a376d2d56e8dee065f9f8b4eb6f91f5b2bd103a
Andru Liu <andruliu2022@u.northwestern.edu>
2022-03-18 00:29:24 -0500

    Completed documentation for send mode

--- code/Send_Mode.py
+++ code/Send_Mode.py
@@ -1,3 +1,23 @@
+"""
+Classifies EMG serial reading into the flexion mode. Uses serialized trained
+models from the Trained_Models directory.
+
+Change line: `filename` to use different ML models.
+
+Modes:
+    0 - no flexion
+    1 - index finger flexed
+    2 - middle finger flexed
+    3 - fourth finger flexed 
+    4 - pinky finger flexed
+
+Models:
+    MLP - multilayer perceptron
+    KNN - K nearest neighbors
+    RFC - random forest classifier
+    
+"""
+
 import serial
 import pickle
 import numpy as np
@@ -5,29 +25,28 @@ from MLP import MLP
 from KNN import KNN
 from RFC import RFC
 
-arduino_port = "/dev/ttyACM2"
+arduino_port = "/dev/ttyACM0"   # Based on port your device is connected to
 baud = 9600
 
 ser = serial.Serial(arduino_port, baud)
 
+# Models to use for classification
 filename = './Trained_Models/KNN.sav'
 # filename = './Trained_Models/RFC.sav'
 # filename = './Trained_Models/MLP.sav'
 model = pickle.load(open(filename, 'rb'))
 
 while True:
-    # read data
+    # Read data
     getData = str(ser.readline().decode())
     data = getData[0:][:-2]
     output = eval(data)
     output = np.array([output])
-    # print(data)
 
     pred, preds, score, _ = model.prediction(output)
-    # print(f"Score: {score}")
     print(f"Prediction: {pred}")
 
-    # write to arduino
+    # Write class to arduino
     if pred == 0:
         ser.write(b'0')
     elif pred == 1:
